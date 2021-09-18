e7a839a7bcc2c328930bbd9f65726c036edfdc3b
DedlySpyder <dedlyspyder@gmail.com>
2021-09-17 22:46:44 -0400

    Mod split for final/diff modes

--- factorio/mods/Factorio_Raw_Data_0.0.1/instrument-after-data.lua
+++ factorio/mods/Factorio_Raw_Data_0.0.1/instrument-after-data.lua
@@ -1,1 +1,7 @@
+local dump = require("__Factorio_Raw_Data__/dump")
+
+log("<<START - FactorioDataRawDump>>")
+log("<<START>><<Factorio_Raw_Data>><<instrument-after-data>>")
+dump()
+log("<<DONE>>")
 log("<<DONE - FactorioDataRawDump>>")
